532ead592436eb5fc3ca823a24a3ef4b49c65391
Bladimir Parra - IP Proyectos y Soluciones <bladyparra@gmail.com>
2023-07-24 22:45:32 -0500

    Commit init server, creación de Modelos y DB

---
+++ api/src/models/Dog.js
@@ -0,0 +1,81 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        noEmpty: true,
+      },
+    },
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: '',
+      validate: {
+        isUrl: true,
+      },
+    },
+    minHeight: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 100,
+      },
+      allowNull: false,
+    },
+    maxHeight: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 100,
+      },
+      allowNull: false,
+    },
+    minWeight: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 100,
+      },
+      allowNull: false,
+    },
+    maxWeight: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 100,
+      },
+      allowNull: false,
+    },
+    minLifeSpan: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 20,
+      },
+      allowNull: false,
+    },
+    maxLifeSpan: {
+      type: DataTypes.INTEGER,
+      validate: { 
+        min: 1, 
+        max: 20, 
+      },
+      allowNull: false,
+    },
+    from: {
+      type: DataTypes.STRING,
+    },
+  },{
+    timestamps: false,
+  });
+};
